4f383b71089418d2e20d4f9b0666991928996e86
MilanR312 <92678773+MilanR312@users.noreply.github.com>
2022-04-29 21:00:09 +0200

    Delete test6-2.py

--- les6/test6-2.py
+++
@@ -1,8 +0,0 @@
-a = getattr(int,dir(int)[32])(int,6)
-b = getattr(int,dir(int)[32])(int,4)
-s = getattr(int,dir(int)[59])(a,b)
-o = getattr(float,dir(int)[62])(s)
-print(o)
-
-o = float.as_integer_ratio(1/10)
-print(o)
